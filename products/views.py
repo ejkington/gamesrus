f8e65042a7067514e4dea7b5c83bc506aabd2f2b
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-03 20:19:21 +0000

    add sorting on products template to better sorting of products

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
-    
+
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -20,29 +20,28 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            
+                if sortkey == 'category':
+                    sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-        
-        
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "No search criteria")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-            
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
