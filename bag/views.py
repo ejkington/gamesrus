020c54cea21234899da074981d124923927c3f1d
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-08 12:22:02 +0000

    add increment and decrement to quantity box with plus and minus sign, also added js to handle it

--- bag/views.py
+++ bag/views.py
@@ -8,11 +8,11 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add items to bag """
-    
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-    
+
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
