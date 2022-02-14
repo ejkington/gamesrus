d7cafff9d495cd87936d7431e9eb55927d0915a7
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-14 20:20:17 +0000

    added oderlineitem models and uuid random number

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import uuid
+from django.db.models import Sum
+from django.conf import settings
+from products.models import Product
 
 
 class Order(models.Model):
@@ -15,4 +19,15 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=15, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=15, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, editable=False)
     
+    
+    def _generate_order_number(self):
+        """ generate random unique order number """
+        return uuid.uuid4(),hex.upper()
