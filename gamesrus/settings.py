be3c862ba9d3dd1744fd71b33da0e2dd0030fd5c
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-14 08:17:15 +0000

    change footer on indexpage and in base.html

--- gamesrus/settings.py
+++ gamesrus/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#269bcdiztnuhl*cenkm98z53rtnaap*du*xqbhuyswm0uggpi'
+SECRET_KEY = 'xj&omd@a=)g4-#zwk7tnpeyf_&=b5jnht6aeo6a6a#w(=tp=5m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
