382950f61bc841dda86c088428704ce3ef98d16b
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-14 12:26:07 +0000

    added update link for updating or removing products from shopping bag

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
+
 
 def view_bag(request):
     """ view for showing the bag contents page """
@@ -11,16 +12,63 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add items to bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    if 'quantity' in request.POST:
+
+        bag = request.session.get('bag', {})
+
+    if quantity:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            del bag[item_id]
+            if not bag[item_id]:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove item from the shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {})
+        quantity = int(request.POST.get('quantity'))
+
+        if quantity:
+            del bag[item_id]
+            if not bag[item_id]:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
