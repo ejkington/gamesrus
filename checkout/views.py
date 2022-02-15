dc932ac89c7f6156e37dfc0ea0abfdb22ea40634
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-15 12:56:11 +0000

    add stripe and customise style

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KTPwkHVIV1508V7B1W0KazbwvKlLB7wm2HUt8Rj19Z94WHWgaX4bt30EfRY0k4RYeb4XjpAzQZ4zzx5H9jxqShI00tMAWvQmt'
+        
     }
 
     return render(request, template, context)
