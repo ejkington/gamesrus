78c7189b9f19dbb451adcf9f27a7648c98c376eb
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-17 17:51:09 +0000

    fixing typo to get webhooks to show in stripe

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,28 +1,32 @@
 from django.http import HttpResponse
 
+
 class StripeWH_Handler:
-    """handles stripe webhooks """
-    
+    """Handle Stripe webhooks"""
+
     def __init__(self, request):
         self.request = request
-        
+
     def handle_event(self, event):
-        """handle a generic/unexpected webhook event"""
-        
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
         return HttpResponse(
-            content=f'unhandled webhook recived: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-        
+
     def handle_payment_intent_succeeded(self, event):
-        """handle payment_intent.succeeded webhok from sprite """
-        
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
         return HttpResponse(
-            content=f'Webhook recived: {event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
-        
+
     def handle_payment_intent_payment_failed(self, event):
-        """handle the payment_intent.payment_failed webhook from sprite"""
-        
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
         return HttpResponse(
-            content=f'Webhook recived: {event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
