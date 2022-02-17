bd9667a478c7b7a02da89fa2127da4b08088d138
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-02-17 17:35:12 +0000

    adding more content to webhooks and trying to get a test webhook from stripe

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,5 +10,19 @@ class StripeWH_Handler:
         """handle a generic/unexpected webhook event"""
         
         return HttpResponse(
+            content=f'unhandled webhook recived: {event["type"]}',
+            status=200)
+        
+    def handle_payment_intent_succeeded(self, event):
+        """handle payment_intent.succeeded webhok from sprite """
+        
+        return HttpResponse(
+            content=f'Webhook recived: {event["type"]}',
+            status=200)
+        
+    def handle_payment_intent_payment_failed(self, event):
+        """handle the payment_intent.payment_failed webhook from sprite"""
+        
+        return HttpResponse(
             content=f'Webhook recived: {event["type"]}',
             status=200)
---
+++ checkout/webhooks.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from django.http import HttpResponse
+
+import stripe
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from checkout.webhook_handler import StripeWH_Handler
+
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """ listens for webhooks from stripe """
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    
+    # gets webbhook data and verify its signature
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+    
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
+        )
+        
+    except ValueError as e:
+        # invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        #invalid signature
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
+    
+    print('Yay you did It')
+    return HttpResponse(status=200)
