f0b9f6cf974fd7b2ed4743066c618547a171668b
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-03-01 12:57:13 +0000

    added login decorators to views to only be accessable by superusers or logged in users

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.contrib.auth.decorators import login_required
+
 from .models import Product, Category
 from .forms import ProductForm
 
@@ -68,8 +70,13 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ Adds product to the store as a superuser """
+    if not request.user.is_superuser:
+        messages.error(request, 'Only store admins can access this page!')
+        return redirect(reverse('home'))
+    
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -89,8 +96,13 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ edits product """
+      if not request.user.is_superuser:
+        messages.error(request, 'Only store admins can access this page!')
+        return redirect(reverse('home'))
+    
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -113,8 +125,13 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
+      if not request.user.is_superuser:
+        messages.error(request, 'Only store admins can access this page!')
+        return redirect(reverse('home'))
+    
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
