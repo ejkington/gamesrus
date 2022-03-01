93a49acaae8f1c50fa0959b3c90ffba66812150f
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-03-01 17:21:56 +0000

    changed allowed host and added configvars in heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gamesruz.wsgi:application
--- gamesrus/settings.py
+++ gamesrus/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'xj&omd@a=)g4-#zwk7tnpeyf_&=b5jnht6aeo6a6a#w(=tp=5m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["gamesruz.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -123,12 +123,19 @@ WSGI_APPLICATION = 'gamesrus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+    
+else:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
