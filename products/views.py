a3e1cc39e73b397eda9e344d4189aadb38907150
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-03-01 09:23:14 +0000

    tested the add product form, added else statement if form isent valid to raise and error message

--- products/views.py
+++ products/views.py
@@ -70,7 +70,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Adds product to the store as a superuser """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product added successfully!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product, check your form and try again!')
+    else:
+        form = ProductForm()
+    
     template = 'products/add_product.html'
     context = {
         'form': form,
