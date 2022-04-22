9841241cbda039aaa1458adb796a727d9edd9f96
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-04-22 09:16:08 +0000

    style the news and news details page

--- news/views.py
+++ news/views.py
@@ -2,9 +2,9 @@ from django.views import generic
 from .models import News
 
 class NewsList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = News.objects.filter(status=1).order_by('-created_on')
     template_name = 'news.html'
 
 class NewsDetail(generic.DetailView):
     model = News
-    template_name = 'News_detail.html'
+    template_name = 'news_detail.html'
