649722d9a9b00b6c82affdc68bf34aaa1e949263
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-04-22 08:37:33 +0000

    added urls and views for the news app

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import News
 
-# Create your views here.
+class NewsList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'news.html'
+
+class NewsDetail(generic.DetailView):
+    model = News
+    template_name = 'News_detail.html'
